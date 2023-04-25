f52b2ec985b540c177c9168865fd2d94319d2f29
ryan-dev05 <rgitau011@gmail.com>
2023-04-25 05:46:09 -0700

    main file

--- main.h
+++ main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
+#include <stddef.h>
 
 int _printf(const char *format, ...);
 #endif
