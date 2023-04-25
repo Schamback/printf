b191cbf279b48ded685127c4c41cda8dbcd7425e
Schamback <kingoeschamback@gmail.com>
2023-04-25 11:17:58 -0700

    Print f

--- printf.c
+++ printf.c
@@ -1,51 +1,100 @@
 #include "main.h"
+#include <stdarg.h>
 
 /**
- * _printf - prints data to stdout
- * @format: string that contains the format to print
- * Return: number of characters written
+ * _printf - prints a formatted string to stdout
+ * @format: string that contains zero or more directives
+ *
+ * Return: the number of characters printed (excluding the null byte used to end output to strings)
  */
-int _printf(char *format, ...)
+int _printf(char *format, ...);
 {
-	int write = 0, (*structype)(char *, va_list);
-	char k[3];
-	va_list ra;
 
-	if (format == NULL)
-		return (-1);
-	k[2] = '\0';
-	va_start(ra, format);
-	_putchar(-1);
-	while (format[0])
-	{
-		if (format[0] == '%')
-		{
-			structype = driver(format);
-			if (structype)
-			{
-				k[0] = '%';
-				k[1] = format[1];
-				write += structype(k, ra);
-			}
-			else if (format[1] != '\0')
-			{
-				write += _putchar('%');
-				write += _putchar(format[1]);
-			}
-			else
-			{
-				write += _putchar('%');
-				break;
-			}
-			format += 2;
-		}
-		else
-		{
-			write += _putchar(format[0]);
-			format++;
-		}
-	}
-	_putchar(-2);
-	return (write);
+	  int i = 0, count = 0;
+    char c;
+    char *s;
+    va_list args;
+
+    va_start(args, format);
+
+    while (format && format[i])
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            switch (format[i])
+            {
+                case 'c':
+                    c = (char) va_arg(args, int);
+                    count += _putchar(c);
+                    break;
+                case 's':
+                    s = va_arg(args, char *);
+                    count += _puts(s);
+                    break;
+                case '%':
+                    count += _putchar('%');
+                    break;
+                default:
+                    count += _putchar('%');
+                    count += _putchar(format[i]);
+                    break;
+            }
+        }
+        else
+        {
+            count += _putchar(format[i]);
+        }
+
+        i++;
+    }
+
+    va_end(args);
+
+    return count;
 }
+
+{
+  va_list args;
+  int sum = 0;
+
+  va_start(args, count);
+  for (int i = 0; i < count; i++) {
+    sum += va_arg(args, int);
+  }
+  va_end(args);
+
+  return sum;
+}
+
+/**
+ * _putchar - writes a character to the standard output stream
+ * @c: the character to write
+ *
+ * Return: the number of characters written (1) or -1 on error
+ */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
+
+/**
+ * _puts - writes a string to the standard output stream
+ * @str: the string to write
+ *
+ * Return: the number of characters written or -1 on error
+ */
+int _puts(char *str)
+{
+    int i = 0;
+
+    while (str && str[i])
+    {
+        _putchar(str[i]);
+        i++;
+    }
+
+    return i;
+}
+
 
