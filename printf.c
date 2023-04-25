28f34ab285b1aa8fe621425808ab80dcbb08b314
ryan-dev05 <rgitau011@gmail.com>
2023-04-25 13:11:59 -0700

    printf

--- printf.c
+++ printf.c
@@ -4,66 +4,64 @@
  * _printf - prints a formatted string to stdout
  * @format: string that contains zero or more directives
  *
- * Return: the number of characters printed (excluding the null byte used to end output to strings)
+ * Return: the number of characters printed
  */
 int _printf(char *format, ...);
 {
+	int k = 0, counts = 0;
 
-	  int k = 0, counts = 0;
-    char y;
-    char *s;
-    va_list args;
+	char y;
+	char *s;
+	va_list args;
 
-    va_start(args, format);
-
-    while (format && format[k])
-    {
-        if (format[k] == '%')
-        {
-            k++;
-            switch (format[k])
-            {
-                case 'y':
-                    y = (char) va_arg(args, int);
-                    counts += _putchar(y);
-                    break;
-                case 's':
-                    s = va_arg(args, char *);
-                    counts += _puts(s);
-                    break;
-                case '%':
-                    counts += _putchar('%');
-                    break;
-                default:
-                    counts += _putchar('%');
-                    counts += _putchar(format[k]);
-                    break;
-            }
-        }
-        else
-        {
-            counts += _putchar(format[k]);
-        }
-
-        k++;
-    }
-
-    va_end(args);
-
-    return counts;
+	va_start(args, format);
+	while (format && format[k])
+	{
+		if (format[k] == '%')
+		{
+			k++;
+			switch (format[k])
+			{
+				case 'y':
+					y = (char) va_arg(args, int);
+					counts += _putchar(y);
+					break;
+				case 's':
+					s = va_arg(args, char *);
+					counts += _puts(s);
+					break;
+				case '%':
+					counts += _putchar('%');
+					break;
+				default:
+					counts += _putchar('%');
+					counts += _putchar(format[k]);
+					break;
+			}
+		}
+		else
+		{
+			counts += _putchar(format[k]);
+		}
+		k++;
+	}
+		va_end(args);
+		return (counts);
 }
 
 {
-  va_list args;
-  int sums = 0;
+	va_list args;
 
-  va_start(args, counts);
-  for (int k = 0; k < counts; k++) {
-    sums += va_arg(args, int);
-  }
-  va_end(args);
+	int sums = 0;
 
-  return sums;
+	va_start(args, counts);
+
+	for (int k = 0; k < counts; k++)
+	{
+		sums += va_arg(args, int);
+	}
+	va_end(args);
+	return (sums);
 }
 
 /**
@@ -74,7 +72,7 @@ int _printf(char *format, ...);
  */
 int _putchar(char c)
 {
-    return write(1, &c, 1);
+	return (write(1, &c, 1));
 }
 
 /**
@@ -85,13 +83,12 @@ int _putchar(char c)
  */
 int _puts(char *str)
 {
-    int u = 0;
+	int u = 0;
 
-    while (str && str[u])
-    {
-        _putchar(str[u]);
-        u++;
-    }
-
-    return u;
+	while (str && str[u])
+	{
+		_putchar(str[u]);
+		u++;
+	}
+	return (u);
 }
