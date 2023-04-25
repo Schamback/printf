766cb904280068d4236afb630572f1f2724e0da8
ryan-dev05 <rgitau011@gmail.com>
2023-04-25 06:13:47 -0700

    files

--- main.h
+++ main.h
@@ -4,6 +4,17 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stddef.h>
+/**
+ * struct printstruct - structure container
+ * @k: method and location to translate data to chars
+ * @u: gives function for specific types
+ * Return: int
+ */
+typedef struct printstruct
+{
+	char *k;
+	int (*u)(char *format, va_list);
+}structype;
 
 int _printf(const char *format, ...);
 #endif
